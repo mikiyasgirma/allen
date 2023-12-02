bb4af383674c14d17996a223f794c18ff27c8d5f
mikiyasgirma <mikiyasgirma43@gmail.com>
2023-12-02 15:02:43 +0300

    fix

--- app/components/course-card.tsx
+++ app/components/course-card.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image"
 import Link from "next/link"
-import { ICourses } from "../courses/page"
 
 const CourseCard = ({thumbnail, title, description, video, id} : any) => {
     
